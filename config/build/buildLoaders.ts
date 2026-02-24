aac44d3aca467e023b6cea6b9ba81ae621ea06e3
dunchek <vasilevaevdokia875@gmail.com>
2026-02-24 10:04:26 +0300

    build: add video & font loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,62 +1,86 @@
-import { ModuleOptions } from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import ReactRefreshTypeScript from "react-refresh-typescript";
-import { BuildOptions } from "./types/types";
+import { ModuleOptions } from 'webpack'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import ReactRefreshTypeScript from 'react-refresh-typescript'
+import { BuildOptions } from './types/types'
 
-export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
-  const isDev = options.mode === "development";
+export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
+  const isDev = options.mode === 'development'
 
-  const styleOrExtract = isDev ? "style-loader" : MiniCssExtractPlugin.loader;
+  const styleOrExtract = isDev ? 'style-loader' : MiniCssExtractPlugin.loader
 
   const accetLoader = {
     test: /\.(png|jpg|jpeg|gif)$/i,
-    type: "asset/resource",
-  };
+    type: 'asset/resource'
+  }
 
   const svgrLoader = {
     test: /\.svg$/i,
     issuer: /\.[jt]sx?$/,
-    use: [{ loader: "@svgr/webpack", options: { icon: true } }],
-  };
+    use: [{ loader: '@svgr/webpack', options: { icon: true } }]
+  }
 
   const scssModuleLoader = {
     test: /\.module\.s[ac]ss$/i,
     use: [
       styleOrExtract,
       {
-        loader: "css-loader",
+        loader: 'css-loader',
         options: {
           esModule: false,
           modules: {
-            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
-          },
-        },
+            localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+          }
+        }
       },
-      "sass-loader",
-    ],
-  };
+      'sass-loader'
+    ]
+  }
 
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     exclude: /\.module\.s[ac]ss$/i,
-    use: [styleOrExtract, "css-loader", "sass-loader"],
-  };
+    use: [styleOrExtract, 'css-loader', 'sass-loader']
+  }
 
   const tsLoader = {
     exclude: /node_modules/,
     test: /\.tsx?$/,
     use: [
       {
-        loader: "ts-loader",
+        loader: 'ts-loader',
         options: {
           transpileOnly: true,
           getCustomTransformers: () => ({
-            before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
-          }),
-        },
-      },
-    ],
-  };
+            before: [isDev && ReactRefreshTypeScript()].filter(Boolean)
+          })
+        }
+      }
+    ]
+  }
 
-  return [accetLoader, scssModuleLoader, scssLoader, tsLoader, svgrLoader];
+  const videoLoader = {
+    test: /\.(mp4|webm|ogg|mov)$/i,
+    type: 'asset/resource',
+    generator: {
+      filename: 'videos/[hash][ext][query]'
+    }
+  }
+
+  const fontLoader = {
+    test: /\.(ttf|otf|woff|woff2)$/i,
+    type: 'asset/resource',
+    generator: {
+      filename: 'fonts/[hash][ext][query]'
+    }
+  }
+
+  return [
+    accetLoader,
+    scssModuleLoader,
+    scssLoader,
+    tsLoader,
+    svgrLoader,
+    videoLoader,
+    fontLoader
+  ]
 }
