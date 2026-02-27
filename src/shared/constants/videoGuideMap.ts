28e6dfc2fb32146a0471902ab9b3ac83d10cdacc
dunchek <vasilevaevdokia875@gmail.com>
2026-02-27 19:39:34 +0300

    feat: add video map (fithout videos)

---
+++ src/shared/constants/videoGuideMap.ts
@@ -0,0 +1,7 @@
+import kendrick from '@/assets/videos/kendrick/Q_vid.mp4';
+
+type videosGuideMapType = Record<string, string>;
+
+export const videosGuideMap: videosGuideMapType = {
+  kendrick: kendrick
+};
