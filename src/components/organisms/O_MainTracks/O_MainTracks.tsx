7a342a4728076ebde343555f726988612bb8eb0b
dunchek <vasilevaevdokia875@gmail.com>
2026-02-28 14:16:09 +0300

    feat: add outer links to YMtrack & to track pic (& upd styles)

--- src/components/organisms/O_MainTracks/O_MainTracks.tsx
+++ src/components/organisms/O_MainTracks/O_MainTracks.tsx
@@ -4,6 +4,8 @@ import classes from '@/components/organisms/O_MainTracks/O_MainTracks.module.scs
 interface Track {
   name: string;
   footer: string;
+  img: string;
+  link: string;
 }
 
 interface Props {
@@ -16,10 +18,18 @@ const O_MainTracks = ({ tracks }: Props) => {
       <h4>Ключевые треки</h4>
       <div className={classes.tracks}>
         {tracks.map((p: Track) => (
-          <div className={classes.track}>
-            <div>{p.name}</div>
-            <div className={classes.footer1}>{p.footer}</div>
-          </div>
+          <a
+            key={p.link}
+            href={p.link}
+            className={classes.track}
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={p.img} className={classes.img}></img>
+            <div className={classes.trackinfo}>
+              <div>{p.name}</div>
+              <div className={classes.footer1}>{p.footer}</div>
+            </div>
+          </a>
         ))}
       </div>
     </div>
