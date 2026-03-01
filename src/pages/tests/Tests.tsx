036127d29f28a55da8cc92056114c60f81f87e51
dunchek <vasilevaevdokia875@gmail.com>
2026-03-01 11:23:20 +0300

    feat: add Tests page (base logic & visual)

--- src/pages/tests/Tests.tsx
+++ src/pages/tests/Tests.tsx
@@ -1,28 +1,36 @@
+import Q_VideoBackground from '@/components/quarks/Q_VideoBackground/Q_VideoBackground';
 import React from 'react';
+import classes from '@/pages/tests/Tests.module.scss';
+import W_SectionElementsWrapper from '@/components/wrappers/W_SectionElementsWrapper/W_SectionElementsWrapper';
+import testInfo from '@/assets/data/tests/tests.json';
+import A_Badge from '@/components/atoms/A_Badge/A_Badge';
+import M_TestCard from '@/components/molecules/M_TestCard/M_TestCard';
 
 const Tests = () => {
+  const link = 'https://dunchek-test-bucket.s3-website.cloud.ru/dubside/videos/tests.mp4';
+
   return (
     <div>
-      <h1>TESTS</h1>
+      <Q_VideoBackground source={link} uppergrad={false} />
 
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        doloremque earum animi hic asperiores sapiente, repellendus velit.
-        Nesciunt animi, aspernatur sequi recusandae temporibus, porro laboriosam
-        autem qui suscipit quaerat dolorum.
-      </p>
-      <p>
-        Ad facere itaque earum corrupti assumenda voluptatum dolorum animi magni
-        cumque doloribus cum illo in molestiae at debitis quae iusto temporibus,
-        ipsum nulla dolor accusamus reprehenderit. Animi nemo accusantium
-        quidem.
-      </p>
-      <p>
-        Eveniet aliquam aperiam dolorum consequuntur error quas adipisci iure
-        animi in harum doloribus saepe nostrum magnam cumque placeat deleniti
-        qui sit, exercitationem, expedita ut! Praesentium tempora dicta
-        voluptate eveniet mollitia.
-      </p>
+      <W_SectionElementsWrapper>
+        <div className={classes.breakdiv}></div>
+        <h1>Подготовили для тебя тесты!</h1>
+        <p>
+          Повеселись с простыми, или попробуй свои силы в более сложных. Главное — ответы на все
+          вопросы можно найти у нас в материалах. Удачи!
+        </p>
+
+        <div className={classes.testswrapper}>
+          <M_TestCard id="1" size="mid" />
+          <M_TestCard id="1" size="mini" />
+          <M_TestCard id="1" size="mini" />
+          <M_TestCard id="1" size="mid" />
+          <M_TestCard id="1" size="max" />
+          <M_TestCard id="1" size="mini" />
+          <M_TestCard id="1" size="mid" />
+        </div>
+      </W_SectionElementsWrapper>
     </div>
   );
 };
