d21a6fd57fac9a550a300b4a8370b1be6d7b5db9
dunchek <vasilevaevdokia875@gmail.com>
2026-03-02 09:50:33 +0300

    feat: add disabled to A_Badge

--- src/components/atoms/A_Badge/A_Badge.tsx
+++ src/components/atoms/A_Badge/A_Badge.tsx
@@ -4,19 +4,24 @@ import classes from '@/components/atoms/A_Badge/A_Badge.module.scss';
 interface Props {
   children: React.ReactNode;
   color?: 'blue' | 'green' | 'red';
+  disabled?: boolean;
 }
 
-const A_Badge = ({ children, color }: Props) => {
+const A_Badge = ({ children, color, disabled }: Props) => {
   let colorClass;
 
   if (!color) {
     colorClass = classes.default;
   } else if (color === 'blue') {
-    colorClass = classes.badge;
+    colorClass = classes.blue;
   } else if (color === 'green') {
     colorClass = classes.green;
   } else {
     colorClass = classes.red;
+  }
+
+  if (disabled) {
+    colorClass = classes.disabled;
   }
 
   return <div className={colorClass}>{children}</div>;
