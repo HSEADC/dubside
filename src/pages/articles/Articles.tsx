1e20c62733dd536b2921df71eafe072d62b0f6fb
dunchek <vasilevaevdokia875@gmail.com>
2026-03-04 08:49:45 +0300

    feat: add base positions logic for Articles page

--- src/pages/articles/Articles.tsx
+++ src/pages/articles/Articles.tsx
@@ -1,14 +1,20 @@
 import Q_VideoBackground from '@/components/quarks/Q_VideoBackground/Q_VideoBackground';
 import W_SectionElementsWrapper from '@/components/wrappers/W_SectionElementsWrapper/W_SectionElementsWrapper';
 import { videosGuideMap } from '@/shared/constants/videoGuideMap';
-import React from 'react';
+import React, { useMemo } from 'react';
 import classes from '@/pages/articles/Articles.module.scss';
 import A_Button from '@/components/atoms/A_Button/A_Button';
 import M_Input from '@/components/molecules/M_Input/M_Input';
-import M_FlipCard from '@/components/molecules/M_FlipCard/M_FlipCard';
+import ArticlesDataRaw from '@/assets/data/articles/articlesInfo.json';
+import Article from '@/shared/types/articles';
+import M_ArticleCard from '@/components/molecules/M_ArticleCard/M_ArticleCard';
+import calcTestCardSize from '@/shared/utils/calcTestCardSize';
 
 const Articles = () => {
   const videoLink = videosGuideMap.hero;
+  const articlesData = useMemo(() => {
+    return ArticlesDataRaw as Article[];
+  }, []);
 
   return (
     <div>
@@ -16,22 +22,19 @@ const Articles = () => {
       <W_SectionElementsWrapper>
         <div className={classes.breakdiv}></div>
 
-        <M_Input placeholder={'бла бsdvsdvла'} />
-
-        {/* <div className={classes.inputwrapp}>
-          <input type="text" className={classes.input} placeholder="впишите слова для поиска" />
-        </div> */}
+        <M_Input placeholder={'впишите слова для поиска'} />
 
         <div className={classes.buttonswrap}>
-          <A_Button isActive={false}>все</A_Button>
-          <A_Button isActive={false}>статьи</A_Button>
-          <A_Button isActive={false}>артисты</A_Button>
-          <A_Button isActive={false}>карточки</A_Button>
+          <A_Button isActive={false}>бронкс</A_Button>
+          <A_Button isActive={false}>история</A_Button>
+          <A_Button isActive={false}>артист</A_Button>
+          <A_Button isActive={false}>раннее</A_Button>
         </div>
 
         <div className={classes.articleswrapper}>
-          <div className={classes.article}></div>
-          <M_FlipCard nickname="kanye" />
+          {articlesData.map((a, i) => {
+            return <M_ArticleCard id={a.id} size={calcTestCardSize(i)} />;
+          })}
         </div>
       </W_SectionElementsWrapper>
     </div>
