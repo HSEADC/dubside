f7e15a5494d8c95a2fc832997ba9253be83db926
dunchek <vasilevaevdokia875@gmail.com>
2026-03-04 19:07:20 +0300

    feat: add Cards page

--- src/pages/cards/Cards.tsx
+++ src/pages/cards/Cards.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import W_SectionElementsWrapper from '@/components/wrappers/W_SectionElementsWrapper/W_SectionElementsWrapper';
-// import classes from '@/pages/cards/Cards.module.scss';
+import Q_VideoBackground from '@/components/quarks/Q_VideoBackground/Q_VideoBackground';
+import { videosGuideMap } from '@/shared/constants/videoGuideMap';
+import classes from '@/pages/cards/Cards.module.scss';
+import M_FlipCard from '@/components/molecules/M_FlipCard/M_FlipCard';
 
 const Cards = () => {
+  const videoLink = videosGuideMap.cards;
   return (
     <div>
-      <section>
-        <W_SectionElementsWrapper>
-          <p>cards</p>
-        </W_SectionElementsWrapper>
-      </section>
+      <Q_VideoBackground source={videoLink} uppergrad={false} />
+      <W_SectionElementsWrapper>
+        <div className={classes.breakdiv}></div>
+
+        <div className={classes.cardswrapper}>
+          <M_FlipCard nickname="kanye" />
+          <M_FlipCard nickname="drake" />
+          <M_FlipCard nickname="kendrick" />
+
+          <M_FlipCard nickname="emenem" />
+          <M_FlipCard nickname="jay-z" />
+          <M_FlipCard nickname="nicki" />
+
+          <M_FlipCard nickname="travis" />
+          <M_FlipCard nickname="50-cent" />
+          <M_FlipCard nickname="carti" />
+
+          <M_FlipCard nickname="future" />
+          <M_FlipCard nickname="wayne" />
+          <M_FlipCard nickname="cole" />
+        </div>
+      </W_SectionElementsWrapper>
     </div>
   );
 };
