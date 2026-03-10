fa70a9fe75baa9263d89a7d8fa9f8273b83c9578
dunchek <vasilevaevdokia875@gmail.com>
2026-03-10 08:30:58 +0300

    feat(Article.tsx): replace video background to image

--- src/pages/article/Article.tsx
+++ src/pages/article/Article.tsx
@@ -1,25 +1,16 @@
-import Q_VideoBackground from '@/components/quarks/Q_VideoBackground/Q_VideoBackground';
 import W_SectionElementsWrapper from '@/components/wrappers/W_SectionElementsWrapper/W_SectionElementsWrapper';
-import { videosGuideMap } from '@/shared/constants/videoGuideMap';
-// import React, { useMemo } from 'react';
 import classes from '@/pages/article/Article.module.scss';
 import { Link, useParams } from 'react-router';
 import { useEffect, useState, useMemo } from 'react';
 import { getArticleContent } from '@/shared/api/articles';
 import { ArticleContent } from '@/shared/types/articles';
 import ArticlesDataRaw from '@/assets/data/articles/articlesInfo.json';
-// import A_Button from '@/components/atoms/A_Button/A_Button';
-// import M_Input from '@/components/molecules/M_Input/M_Input';
-// import ArticlesDataRaw from '@/assets/data/articles/articlesInfo.json';
 import { Article, ArticleBlockContent } from '@/shared/types/articles';
 import O_ArticleSection from '@/components/organisms/O_ArticleSection/O_ArticleSection';
 import { calcArticleSide } from '@/shared/utils/calcArticleSide';
-// import M_ArticleCard from '@/components/molecules/M_ArticleCard/M_ArticleCard';
-// import calcTestCardSize from '@/shared/utils/calcTestCardSize';
+import Q_ImageBackground from '@/components/quarks/Q_ImageBackground/Q_ImageBackground';
 
 const Article = () => {
-  const videoLink = videosGuideMap.hero;
-
   const params = useParams();
   const slug = params.slug;
 
@@ -27,14 +18,20 @@ const Article = () => {
     return ArticlesDataRaw as Article[];
   }, []);
 
-  const articleInfo: Article | undefined = articlesData.find(
+  const articleInfo: Article | null | undefined = articlesData.find(
     (value: Article) => value.slug === slug
   );
-  if (typeof articleInfo === 'undefined') throw new Error('Статья не найдена');
+  if (typeof articleInfo === 'undefined') {
+    console.log('Статья не найдена');
+    throw new Error('Статья не найдена');
+  }
 
   const [articleContent, setArticleContent] = useState<ArticleContent | null>(null);
   const contents = useMemo(() => {
-    if (!articleContent) return [];
+    if (!articleContent) {
+      console.log('Нет контента');
+      return [];
+    }
     return articleContent
       .map((section, index) => ({ id: String(index), heading: section.heading }))
       .filter((item) => item.heading !== '');
@@ -48,10 +45,11 @@ const Article = () => {
   }, [articleInfo.contentLink]);
 
   console.log(articleContent, articleInfo);
+  const imgLink = articleInfo.img;
 
   return (
     <div>
-      <Q_VideoBackground source={videoLink} uppergrad={false} />
+      <Q_ImageBackground source={imgLink} uppergrad={false} />
       <W_SectionElementsWrapper>
         <div className={classes.breakdiv}></div>
         <h2>{articleInfo.title}</h2>
@@ -71,7 +69,12 @@ const Article = () => {
 
         {articleContent?.map((section: ArticleBlockContent, i: number) => {
           return (
-            <O_ArticleSection key={String(i)} section={section} side={calcArticleSide(i)} index={String(i)} />
+            <O_ArticleSection
+              key={String(i)}
+              section={section}
+              side={calcArticleSide(i)}
+              index={String(i)}
+            />
           );
         })}
       </W_SectionElementsWrapper>
