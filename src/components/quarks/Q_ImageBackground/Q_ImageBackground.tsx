44457abc26d5f829ecd0bf3f1f64f3afd63fe3a1
dunchek <vasilevaevdokia875@gmail.com>
2026-03-10 08:28:27 +0300

    feat: add Q_ImageBackground component

---
+++ src/components/quarks/Q_ImageBackground/Q_ImageBackground.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import effect from '@/assets/images/Q_HeroEffect.png';
+import classes from '@/components/quarks/Q_ImageBackground/Q_ImageBackground.module.scss';
+
+interface Q_ImageBackground {
+  source: string;
+  uppergrad: boolean;
+}
+
+const Q_ImageBackground = ({ source, uppergrad }: Q_ImageBackground) => {
+  return (
+    <div className={classes.wrapper}>
+      <img src={source} className={classes.vid} />
+      <div className={classes.gradientdown}></div>
+      <div className={uppergrad ? classes.gradientup : undefined}></div>
+    </div>
+  );
+};
+
+export default Q_ImageBackground;
