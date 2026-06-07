2b45d68d4d71383495eb43cd35e72864e6bd61ad
dunchek <vasilevaevdokia875@gmail.com>
2026-06-07 11:07:28 +0300

    fix: add outer classname in A_ImgSmall.tsx

--- src/components/atoms/A_ImgSmall/A_ImgSmall.tsx
+++ src/components/atoms/A_ImgSmall/A_ImgSmall.tsx
@@ -4,11 +4,12 @@ import classes from '@/components/atoms/A_ImgSmall/A_ImgSmall.module.scss';
 type Props = {
   source: string;
   footer: string;
+  wrapperClassName?: string;
 } & React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
 
-const A_ImgSmall = ({ source, footer, ...rest }: Props) => {
+const A_ImgSmall = ({ source, footer, wrapperClassName, ...rest }: Props) => {
   return (
-    <div className={classes.imgwrapper}>
+    <div className={[classes.imgwrapper, wrapperClassName].filter(Boolean).join(' ')}>
       <img src={source} alt="side-img" {...rest} className={classes.img} />
       <div className={classes.grad}></div>
       <span className={classes.footer}>{footer}</span>
