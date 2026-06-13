52cda573597d8a7a3228e32c5c21dee870e96905
dunchek <vasilevaevdokia875@gmail.com>
2026-06-13 22:23:53 +0300

    feat: add Q_SpecialProjectGradient.tsx component to the SpecialProject page, all working (SpecialProject.tsx)

--- src/pages/special-project/SpecialProject.tsx
+++ src/pages/special-project/SpecialProject.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import W_SectionElementsWrapper from '@/components/wrappers/W_SectionElementsWrapper/W_SectionElementsWrapper';
 import classes from '@/pages/special-project/SpecialProject.module.scss';
+import Q_SpecialProjectGradient from '@/components/quarks/Q_SpecialProjectGradient/Q_SpecialProjectGradient';
 
 const SpecialProject = () => {
   return (
-    <div>
-      <W_SectionElementsWrapper>
+    <div className={classes.page}>
+      <Q_SpecialProjectGradient />
+      <W_SectionElementsWrapper className={classes.content}>
         <div className={classes.breakdiv}></div>
         <h1>test</h1>
       </W_SectionElementsWrapper>
