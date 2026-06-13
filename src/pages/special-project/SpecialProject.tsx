28ace91e11247f84fb2f28ba74f60904f7d4c36b
dunchek <vasilevaevdokia875@gmail.com>
2026-06-13 21:29:53 +0300

    feat: add main SpecialProject.tsx files

---
+++ src/pages/special-project/SpecialProject.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import W_SectionElementsWrapper from '@/components/wrappers/W_SectionElementsWrapper/W_SectionElementsWrapper';
+import classes from '@/pages/special-project/SpecialProject.module.scss';
+
+const SpecialProject = () => {
+  return (
+    <div>
+      <W_SectionElementsWrapper>
+        <div className={classes.breakdiv}></div>
+        <h1>test</h1>
+      </W_SectionElementsWrapper>
+    </div>
+  );
+};
+
+export default SpecialProject;
